5ca7f7dbbe6741039f2559da48e5331535da4311
xpf <2502939456@qq.com>
2023-03-07 13:02:14 +0800

    v1

---
+++ src/main/java/com/example/gitspring/GitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitspring/GitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
